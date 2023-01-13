8c15caca8c572c438f7260b9209abdd36acb68db
wangyu <2514825513@qq.com>
2023-01-13 14:47:40 +0800

    MOD: modify video dir

--- bin/index.js
+++ bin/index.js
@@ -60,26 +60,30 @@ async function downloadVideo(url, name) {
         })
     })
 }
-async function downloadAll(mp3Url, mp4Url, name) {
+async function downloadAll(mp3Url, mp4Url, name, dir) {
     if (!fs.existsSync('./_video')) {
         fs.mkdirSync('./_video')
     }
-    if (!fs.existsSync('./video')) {
-        fs.mkdirSync('./video')
+    if (!fs.existsSync(dir ? dir : './video')) {
+        fs.mkdirSync(dir ? dir : './video')
     }
     await Promise.all([downloadMp3(mp3Url, name),downloadVideo(mp4Url, name)])
     //ffmpeg -i ./_video/${name}.mp4 -i ./_video/${name}.mp3 -c copy ./video/${name}.mp4
-    child_process.exec(`ffmpeg -i ./_video/${name}.mp4 -i ./_video/${name}.mp3 -c copy ./video/${name}.mp4`,function(error, stdout, stderr){
-        if(error){
-            console.error("合成失败---",error);
-        }else{
-            console.log("合成成功--",stdout);
-        }
-        // 删除临时文件
-        // child_process.exec('rm -rf ./_video', () => {
-        //   console.log('清理_video文件夹成功');
-        // })
-    });
+    return new Promise((resolve, reject) => {
+        child_process.exec(`ffmpeg -i ./_video/${name}.mp4 -i ./_video/${name}.mp3 -c copy ${dir ? dir : './video'}/${name}.mp4`,function(error, stdout, stderr){
+            if(error){
+                console.error("合成失败---",error);
+                reject(error)
+            }else{
+                console.log("合成成功--",stdout);
+                resolve()
+            }
+            // 删除临时文件
+            // child_process.exec('rm -rf ./_video', () => {
+            //   console.log('清理_video文件夹成功');
+            // })
+        });        
+    })
 }
 // downloadALL(
 //     'https://upos-sz-mirrorcosb.bilivideo.com/upgcxcode/66/81/779808166/779808166_nb3-1-30232.m4s?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M=&uipk=5&nbs=1&deadline=1673521419&gen=playurlv2&os=cosbbv&oi=0&trid=f713be2143774bc0997359a7cc6b70dau&mid=194162133&platform=pc&upsig=b300d2c9d6e9e27a142051506d6249f3&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&bvc=vod&nettype=0&orderid=2,3&buvid=59A23B71-E8AE-42A5-BEB2-30ABB617AA28143082infoc&build=0&agrr=1&bw=16663&logo=40000000',
--- download-bilibili.js
+++ download-bilibili.js
@@ -2,7 +2,7 @@ const { downloadAll } = require('./bin/index')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const {headers} = require('./config')
-async function resolveBilibili(url) {
+async function resolveBilibili(url, name, dir) {
    let res = await axios({
         url,
         headers,
@@ -11,12 +11,27 @@ async function resolveBilibili(url) {
    console.log(res.data);
     $ = cheerio.load(res.data);
     const title = $('title').text().replace(/[\s\\\/]/g, '')
-    // let json = /(?<=<script>) & (?=<\/script>)/.exec(res.data)
+    //可以使用预查直接一步到位获取目标字符串
+    // let json = /(?<=<script>).*?(?=<\/script>)/.exec(res.data)
     let json = /<script>(.*?)<\/script>/.exec(res.data)
     json = json[1].match(/window.__playinfo__=(.*)/)[1]
     json = JSON.parse(json)
-    downloadAll(json.data.dash.audio[0].backup_url[1], json.data.dash.video[0].backup_url[1], title)
+    return downloadAll(
+        json.data.dash.audio[0].backup_url[1], 
+        json.data.dash.video[0].backup_url[1], 
+        name ? name : title, 
+        dir
+    )
 }
 resolveBilibili(
-    'https://www.bilibili.com/video/BV1z8411p7JU/?spm_id_from=333.1007.tianma.2-1-3.click'
+    'https://www.bilibili.com/video/BV1z8411p7JU/?spm_id_from=333.1007.tianma.2-1-3.click',
+    null,
+    './video'    
+).then(
+    () => {
+        console.log('爬取成功');
+    },
+    (error) => {
+        console.log(error);
+    }
 )
